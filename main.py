2cf73d7ac34fed30b5d299979824af4e952bbe0c
seiya8 <seiya.sasa8@gmail.com>
2023-04-25 00:24:02 +0900

    add detector select box

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from pycbc.waveform import get_td_waveform
+from pycbc.detector import Detector
 from dash import Dash, dcc, html, Input, Output
 from callbacks import *
 import dash_bootstrap_components as dbc
 import pandas
 import numpy as np
 import plotly.express as px
+import plotly.graph_objs as go
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, 'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -24,14 +26,15 @@ app.layout = html.Div(
                             **Detectors**
                         """),
                         dcc.Dropdown(
-                            id = 'det',
+                            id = 'dets',
                             options = [
                                 {'label': 'H1', 'value': 'H1'},
                                 {'label': 'L1', 'value': 'L1'},
                                 {'label': 'V1', 'value': 'V1'},
                                 {'label': 'K1', 'value': 'K1'}
                             ],
-                            multi = True
+                            multi = True,
+                            value = ['H1', 'L1']
                         ),
                     ]
                 ),
@@ -55,6 +58,35 @@ app.layout = html.Div(
                             tooltip={'placement': 'bottom', 'always_visible': True}
                         )
                     ]
+                ),
+                html.Div(
+                    className='three columns',
+                    children=[
+                        dcc.Markdown("""
+                            **Declination**
+                        """),
+                        dcc.Slider(
+                            id='dec',
+                            min=0, max=3.14, step=0.01, value=0, marks=None,
+                            tooltip={'placement': 'bottom', 'always_visible': True}
+                        ),
+                        dcc.Markdown("""
+                            **Right Ascension**
+                        """),
+                        dcc.Slider(
+                            id='ra',
+                            min=0, max=6.28, step=0.01, value=0, marks=None,
+                            tooltip={'placement': 'bottom', 'always_visible': True}
+                        ),
+                        dcc.Markdown("""
+                            **Polarization angle**
+                        """),
+                        dcc.Slider(
+                            id='pol',
+                            min=0, max=6.28, step=0.01, value=0, marks=None,
+                            tooltip={'placement': 'bottom', 'always_visible': True}
+                        )
+                    ]
                 )
             ]
         ),
@@ -66,21 +98,34 @@ app.layout = html.Div(
 
 @app.callback(
     Output('plot', 'figure'),
-    Input('det', 'value'),
+    Input('dets', 'value'),
     Input('mass1', 'value'),
-    Input('mass2', 'value')
+    Input('mass2', 'value'),
+    Input('dec', 'value'),
+    Input('ra', 'value'),
+    Input('pol', 'value'),
 )
-def update_figure(det, mass1, mass2):
-    hp, _ = get_td_waveform(
+def update_figure(dets, mass1, mass2, dec, ra, pol):
+    hp, hc = get_td_waveform(
         approximant='SEOBNRv4',
         mass1=int(mass1),
         mass2=int(mass2),
-        f_lower=15,
+        f_lower=10,
         delta_t=1/2048
     )
-    fig = px.line(x=np.array(hp.sample_times), y=np.array(hp))
+    wave_dict = {det: Detector(det).project_wave(hp, hc, ra, dec, pol) for det in ('H1', 'L1', 'V1', 'K1')}
+
+    wave_plots = []
+    for det in dets:
+        wave_plots.append(
+            go.Scatter(
+                x=np.array(wave_dict[det].sample_times),
+                y=np.array(wave_dict[det]),
+                name=det
+            )
+        )
+    fig = go.Figure(wave_plots)
     fig.update_layout(
-        title=f'{det}',
         xaxis_title='Time from merger (s)',
         yaxis_title='Strain'
     )
